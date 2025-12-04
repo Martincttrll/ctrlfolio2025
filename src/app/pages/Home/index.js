3f24e95f7a803ca9c2ef438307e4e1115ab9c92d
unknown <m.cottrelle@mediacall.fr>
2025-12-04 16:04:04 +0100

    feat: h2 text-columns + hero info

--- src/app/pages/Home/index.js
+++ src/app/pages/Home/index.js
@@ -17,8 +17,35 @@ export default class Home extends Page {
 
   create() {
     super.create();
+    this.initHeroInfo();
     this.createWorkViewer();
     this.createLab();
+  }
+
+  initHeroInfo() {
+    const pad = (n) => n.toString().padStart(2, "0");
+
+    const dateEl = document.querySelector('[data-info="date"]');
+    const timestampEl = document.querySelector('[data-info="timestamp"]');
+
+    const updateClock = () => {
+      const now = new Date();
+
+      const YYYY = new Date().getFullYear();
+      const MM = pad(now.getMonth() + 1);
+      const DD = pad(now.getDate());
+      const hh = pad(now.getHours());
+      const mm = pad(now.getMinutes());
+      const ss = pad(now.getSeconds());
+
+      const timestamp = Math.floor(now.getTime() / 1000);
+
+      dateEl.textContent = `${YYYY}-${MM}-${DD} ${hh}:${mm}:${ss}`;
+      timestampEl.textContent = `[${timestamp}]`;
+    };
+
+    updateClock();
+    setInterval(updateClock, 1000);
   }
 
   createWorkViewer() {
